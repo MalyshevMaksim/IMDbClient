3320faf94424a42719aebc2dc98697c9e74f97fa
Malyshev Maksim <malyshev.maksim99@gmail.com>
2020-09-02 07:29:10 +0300

    Initial Commit

---
+++ IMDbAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IMDbAPI
+//
+//  Created by Малышев Максим Алексеевич on 9/2/20.
+//  Copyright © 2020 Малышев Максим Алексеевич. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
