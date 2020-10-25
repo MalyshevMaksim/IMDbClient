a81af662e8a81569097f6f967b717e018a6fb2e4
Malyshev Maksim <malyshev.maksim99@gmail.com>
2020-10-25 06:44:46 +0300

    Adding tests for SearchPresenter

--- IMDbClientTests/PresenterTests/DetailPresenterTest.swift
+++ IMDbClientTests/PresenterTests/DetailPresenterTest.swift
@@ -7,27 +7,123 @@
 //
 
 import XCTest
+@testable import IMDbClient
+
+class DetailStubViewController: UIViewController, DetailViewControllerProtocol {
+    var presenter: MovieDetailPresenterProtocol!
+    var isSuccessCalled = false
+    var isFailureCalled = false
+    
+    func success() {
+        isSuccessCalled = true
+    }
+    
+    func failure(error: Error) {
+        isFailureCalled = true
+    }
+}
+
+class DetailViewStub: UIViewController, MovieDetailViewProtocol {
+    var image: UIImage?
+    var movieTitle: String?
+    var imDbRating: String?
+    var length: String?
+    var releaseDate: String?
+    var contentRating: String?
+    var plot: String?
+    
+    func display(image: UIImage?) {
+        self.image = image
+    }
+    
+    func display(title: String) {
+        self.movieTitle = title
+    }
+    
+    func display(imDbRating: String) {
+        self.imDbRating = imDbRating
+    }
+    
+    func display(length: String) {
+        self.length = length
+    }
+    
+    func display(releaseDate: String) {
+        self.releaseDate = releaseDate
+    }
+    
+    func display(contentRating: String) {
+        self.contentRating = contentRating
+    }
+    
+    func display(plot: String) {
+        self.plot = plot
+    }
+}
 
 class DetailPresenterTest: XCTestCase {
+    var viewStub: DetailStubViewController!
+    var networkMock: NetworkServiceMock!
+    var requestStub: APIRequest!
+    var cacheStub: CacheGatewayStub!
+    var sut: MovieDetailPresenter!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        viewStub = DetailStubViewController()
+        networkMock = NetworkServiceMock(dataStub: nil)
+        cacheStub = CacheGatewayStub()
+        requestStub = APIRequestMock(state: .success)
+        sut = MovieDetailPresenter(view: viewStub, networkService: networkMock, resource: requestStub, cache: cacheStub)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        super.tearDown()
+        viewStub = nil
+        networkMock = nil
+        cacheStub = nil
+        requestStub = nil
+        sut = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testConfigureView() {
+        let movieStub = Movie(id: "foo", title: "foo", image: "foo", crew: "foo", plot: "foo", description: "foo", imDbRating: "foo", imDbRatingCount: "foo", runtimeStr: "foo", year: "foo", releaseDate: "foo", contentRating: "foo")
+        let view = DetailViewStub()
+        sut.movieDetail = movieStub
+        sut.configureView(view: view)
+        
+        XCTAssertEqual(movieStub.title, view.movieTitle)
+        XCTAssertEqual(movieStub.imDbRating, view.imDbRating)
+        XCTAssertEqual(movieStub.runtimeStr, view.length)
+        XCTAssertEqual(movieStub.releaseDate, view.releaseDate)
+        XCTAssertEqual(movieStub.plot, view.plot)
+        XCTAssertEqual(movieStub.contentRating, view.contentRating)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testDownloaDetailExecuteSuccess() {
+        let movieStub = Movie(id: "1", title: "foo", image: "bar", crew: "baz")
+        networkMock.dataStub = try! JSONEncoder().encode(movieStub)
+        sut.downloadMovieDetail()
+        XCTAssertTrue(viewStub.isSuccessCalled)
+        XCTAssertTrue(cacheStub.isMovieCached)
+        XCTAssertEqual(sut.movieDetail, movieStub)
     }
-
+    
+    func testDownloaDetailExecuteFailure() {
+        networkMock.dataStub = nil
+        sut.downloadMovieDetail()
+        XCTAssertTrue(viewStub.isFailureCalled)
+        XCTAssertNil(sut.movieDetail)
+    }
+    
+    func testDownloaDetailExecuteRequestFail() {
+        let failureResourceStub = APIRequestMock(state: .failure)
+        sut.resource = failureResourceStub
+        viewStub.isFailureCalled = false
+        sut.downloadMovieDetail()
+        
+        XCTAssertNil(sut.movieDetail)
+        XCTAssertFalse(viewStub.isFailureCalled)
+        XCTAssertFalse(viewStub.isSuccessCalled)
+    }
 }
